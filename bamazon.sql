103057964bbe8a78dd92af0c0b4261cf86603025
AldenM <bleedintrueblue33@yahoo.com>
2019-05-22 21:11:46 -0700

    Created Database and Javascript code

---
+++ bamazon.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products (
+   id INTEGER(11) AUTO_INCREMENT NOT NULL,
+   product_name VARCHAR(50),
+   department_name VARCHAR(50),
+   price VARCHAR(50),
+   stock_quantity VARCHAR(50),
+   PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Smartphone”, “Electronics”, “500”, “100”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Bedding”, “Home Decor”, “100”, “75”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Game Console”, “Electronics”, “350”, “50”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Rugs”, “Home Decor”, “50”, “750”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Smartphone”, “Electronics”, “499”, “100”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Knives”, “Kitchen”, “150”, “50”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Ovens”, “Kitchen”, “800”, “25”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Grills”, “Outdoor”, “200”, “15”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Furniture”, “Outdoor”, “100”, “400”);
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (“Decals”, “Automobile”, “20”, “600”);
---
+++ bamazonCustomer.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    queryAllProducts();
+
+function queryAllProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      for (var i = 0; i < res.length; i++) {
+        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+      }
+      console.log("-----------------------------------");
+    });
+};
